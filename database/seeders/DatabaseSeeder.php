0a5f4e210ffc2b73a1cf9f82ef6dec49798e2336
Ahmad Azwar Annas <ahmadazw2@gmail.com>
2021-08-27 11:28:56 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $categories = ['Desain', 'Edukasi', 'Keluarga & Anak', 'Kesehatan', 'Makanan & Minuman', 'Otomotif', 'Peliharaan', 'Permainan', 'Politik', 'Teknologi', 'Travel'];
+
+        foreach($categories as $category)
+        {
+            DB::table('categories')->insert([
+                'name'  =>  $category
+            ]);
+        }
     }
 }
